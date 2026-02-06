197f84f1bfa3727ff0e8f60be122435fe53c14be
G00dS0ul <iseoluwaolowogoke@student.oauife.edu.ng>
2026-02-06 02:47:50 +0100

    Implement the jump logic and the king logic

--- G00DS0ULCHECKERS/G00DS0ULCHECKERS.ViewModel/GameSession.cs
+++ G00DS0ULCHECKERS/G00DS0ULCHECKERS.ViewModel/GameSession.cs
@@ -52,8 +52,6 @@ namespace G00DS0ULCHECKERS.ViewModel
         {
             if (parameter is Square clickedSquare)
             {
-                System.Diagnostics.Debug.WriteLine($"Clicked Square: {clickedSquare.Row}, {clickedSquare.Column}");
-
                 if (clickedSquare.CurrentPiece != null && clickedSquare.CurrentPiece.Color == CurrentPlayerTurn)
                 {
                     if (SelectedSquare != null)
@@ -63,21 +61,16 @@ namespace G00DS0ULCHECKERS.ViewModel
 
                     SelectedSquare = clickedSquare;
                     SelectedSquare.IsSelected = true;
-                    System.Diagnostics.Debug.WriteLine("Piece Selected");
-
-                    System.Diagnostics.Debug.WriteLine($"Selected: {clickedSquare.Row}, {clickedSquare.Column}");
                 }
                 else if (SelectedSquare != null)
                 {
-                    System.Diagnostics.Debug.WriteLine($"Attempting move from {SelectedSquare.Row},{SelectedSquare.Column} to {clickedSquare.Row},{clickedSquare.Column}....");
                     if (IsValidSimpleMove(SelectedSquare, clickedSquare))
                     {
-                        System.Diagnostics.Debug.WriteLine("Move Valid! Executing...");
                         MovePiece(SelectedSquare, clickedSquare);
                     }
-                    else
+                    else if(IsValidJumpMove(SelectedSquare, clickedSquare))
                     {
-                        System.Diagnostics.Debug.WriteLine("Move Rejected by Logic");
+                        MovePiece(SelectedSquare, clickedSquare);
                     }
                 }
             }
@@ -88,7 +81,6 @@ namespace G00DS0ULCHECKERS.ViewModel
             // Rule 1: The target square must be empty
             if (to.CurrentPiece != null)
             {
-                System.Diagnostics.Debug.WriteLine("Fail: Target is not Empty");
                 return false;
             }
 
@@ -96,38 +88,101 @@ namespace G00DS0ULCHECKERS.ViewModel
             var colDiff = Math.Abs(to.Column - from.Column);
             if (colDiff != 1)
             {
-                System.Diagnostics.Debug.WriteLine($"Fail: Column diff is {colDiff} (Must be 1).");
                 return false;
             }
 
             // Rule 3: Must move Forward, Red moves up (Row -1), white moves Down (Row +1)
              var rowDiff = to.Row - from.Row;
 
+             // If it is a king, It can go any direction
+             if (from.CurrentPiece?.IsKing ?? false)
+             {
+                 return Math.Abs(rowDiff) == 1;
+             }
+
              if (from.CurrentPiece?.Color == PlayerColor.Red)
              {
                  if (rowDiff == -1) return true;
-                 System.Diagnostics.Debug.WriteLine($"Fail: Red row diff is {rowDiff} (Must be -1");
              }
              else //white
              {
                  if (rowDiff == 1) return true;
-                 System.Diagnostics.Debug.WriteLine($"Fail: White row diff is {rowDiff} (Must be 1");
              }
 
              return false;
         }
 
+        private bool IsValidJumpMove(Square from, Square to)
+        {
+            //Target must be Empty
+            if (to.CurrentPiece != null) return false;
+
+            // Geometry: Must move exactly 2 square diagonally
+            var rowDiff = to.Row - from.Row;
+            var colDiff = to.Column - from.Column;
+
+            if (Math.Abs(rowDiff) != 2 || Math.Abs(colDiff) != 2) return false;
+
+            // Find the victim (the piece in between)
+            var midRow = (from.Row + to.Row) / 2;
+            var midCol = (from.Column + to.Column) / 2;
+            var victimPiece = CurrentBoard.Grid[midRow, midCol];
+
+            // there must be a victim, and it must be the second color
+            if (victimPiece == null) return false;
+            if (victimPiece.Color == from.CurrentPiece?.Color) return false;
+
+            // Direction (Red moves up while White moves down) Todo: Add King
+            if (from.CurrentPiece?.IsKing ?? false)
+            {
+                return Math.Abs(rowDiff) == 2;
+            }
+
+            if (from.CurrentPiece?.Color == PlayerColor.Red)
+            {
+                if (rowDiff != -2) return false; // Red must move up by -2
+            }
+            else //White
+            {
+                if (rowDiff != 2) return false; //White must move up by +2
+            }
+
+            return true;
+        }
+
         private void MovePiece(Square from, Square to)
         {
+            if (Math.Abs(to.Row - from.Row) == 2)
+            {
+                var midRow = (from.Row + to.Row) / 2;
+                var midCol = (from.Column + to.Column) / 2;
+
+                // Kill The Enemy!!
+                CurrentBoard.Grid[midRow, midCol] = null;
+            }
+
             var p = CurrentBoard.Grid[from.Row, from.Column];
+            if (p != null)
+            {
+                CurrentBoard.Grid[to.Row, to.Column] = p;
+                CurrentBoard.Grid[from.Row, from.Column] = null;
+            }
+            
 
-            CurrentBoard.Grid[to.Row, to.Column] = p;
-            CurrentBoard.Grid[from.Row, from.Column] = null;
+            // The Coronation Ceremony
+            if (p?.Color == PlayerColor.Red && to.Row == 0)
+            {
+                p.IsKing = true;
+                System.Diagnostics.Debug.WriteLine("A Red King is Crowned!!");
+            }
 
-            if (CurrentPlayerTurn == PlayerColor.Red)
-                CurrentPlayerTurn = PlayerColor.White;
-            else
-                CurrentPlayerTurn = PlayerColor.Red;
+            else if (p.Color == PlayerColor.White && to.Row == 7)
+            {
+                p.IsKing = true;
+                System.Diagnostics.Debug.WriteLine("A White king is Crown!!!");
+            }
+
+            CurrentPlayerTurn = CurrentPlayerTurn == PlayerColor.Red ? PlayerColor.White : PlayerColor.Red;
 
             TurnMessage = $"{CurrentPlayerTurn}'s Turn";
 
